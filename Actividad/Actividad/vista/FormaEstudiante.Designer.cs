bfc4751ca9588762fc5603fabf7577046cbf1ba7
fabya <fabya@DESKTOP-C1L5572>
2019-03-12 13:32:56 -0600

    Se agrego foma

---
+++ Actividad/Actividad/vista/FormaEstudiante.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Actividades.vista
+{
+    partial class FormaEstudiante
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonMostrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textoNumeroControl = new System.Windows.Forms.TextBox();
+            this.textoNombre = new System.Windows.Forms.TextBox();
+            this.textoEdad = new System.Windows.Forms.TextBox();
+            this.textGenero = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonMostrar
+            // 
+            this.buttonMostrar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMostrar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonMostrar.Location = new System.Drawing.Point(436, 293);
+            this.buttonMostrar.Name = "buttonMostrar";
+            this.buttonMostrar.Size = new System.Drawing.Size(103, 33);
+            this.buttonMostrar.TabIndex = 0;
+            this.buttonMostrar.Text = "Mostrar";
+            this.buttonMostrar.UseVisualStyleBackColor = false;
+            this.buttonMostrar.Click += new System.EventHandler(this.buttonMostrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(82, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(394, 57);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Datos de Estudiante";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(12, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "No. Control";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(12, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(12, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 32);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Edad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(12, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 32);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Gènero";
+            // 
+            // textoNumeroControl
+            // 
+            this.textoNumeroControl.Location = new System.Drawing.Point(220, 109);
+            this.textoNumeroControl.Name = "textoNumeroControl";
+            this.textoNumeroControl.Size = new System.Drawing.Size(303, 20);
+            this.textoNumeroControl.TabIndex = 6;
+            // 
+            // textoNombre
+            // 
+            this.textoNombre.Location = new System.Drawing.Point(220, 150);
+            this.textoNombre.Name = "textoNombre";
+            this.textoNombre.Size = new System.Drawing.Size(303, 20);
+            this.textoNombre.TabIndex = 7;
+            // 
+            // textoEdad
+            // 
+            this.textoEdad.Location = new System.Drawing.Point(220, 194);
+            this.textoEdad.Name = "textoEdad";
+            this.textoEdad.Size = new System.Drawing.Size(303, 20);
+            this.textoEdad.TabIndex = 8;
+            // 
+            // textGenero
+            // 
+            this.textGenero.Location = new System.Drawing.Point(220, 241);
+            this.textGenero.Name = "textGenero";
+            this.textGenero.Size = new System.Drawing.Size(303, 20);
+            this.textGenero.TabIndex = 9;
+            // 
+            // FormaEstudiante
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(551, 338);
+            this.Controls.Add(this.textGenero);
+            this.Controls.Add(this.textoEdad);
+            this.Controls.Add(this.textoNombre);
+            this.Controls.Add(this.textoNumeroControl);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonMostrar);
+            this.Name = "FormaEstudiante";
+            this.Text = "FormaEstudiante";
+            this.Load += new System.EventHandler(this.FormaEstudiante_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonMostrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textoNumeroControl;
+        private System.Windows.Forms.TextBox textoNombre;
+        private System.Windows.Forms.TextBox textoEdad;
+        private System.Windows.Forms.TextBox textGenero;
+    }
+}
---
+++ Actividad/Actividad/vista/FormaEstudiante.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Actividades.modelo;
+
+namespace Actividades.vista
+{
+    public partial class FormaEstudiante : Form
+    {
+        public FormaEstudiante()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormaEstudiante_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonMostrar_Click(object sender, EventArgs e)
+        {
+            Estudiante student01 = new Estudiante();
+            student01.NumeroControl = 1218100604;
+            student01.Nombre = "Fabian Hernandez Guerrero";
+            student01.Edad = 22;
+            student01.Genero = 'H';
+
+            textoNumeroControl.Text = student01.NumeroControl.ToString();
+            textoNombre.Text = student01.Nombre;
+            textoEdad.Text = student01.Edad.ToString();
+            textGenero.Text = student01.Genero.ToString();
+        }
+    }
+}
